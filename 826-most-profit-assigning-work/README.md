2efa1e5c8b03d170eb652bec98121b89ca785dd1
Krisna <ejikrisnayana@gmail.com>
2024-06-18 08:45:26 +0700

    826. Most Profit Assigning Work

---
+++ 826-most-profit-assigning-work/README.md
@@ -0,0 +1,3 @@
+## 826. Most Profit Assigning Work
+
+Question link: https://leetcode.com/problems/most-profit-assigning-work/description/
---
+++ 826-most-profit-assigning-work/src/Solution.java
@@ -0,0 +1,35 @@
+public class Solution {
+    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        int maxDifficulty = 0;
+        // find maximum difficulty of all job
+        for (int diff: difficulty) {
+            maxDifficulty = Math.max(maxDifficulty, diff);
+        }
+
+        // find max profit if there is duplicate difficulty with different profit
+        int[] bestProfit = new int[maxDifficulty + 1];
+        for (int i = 0; i < profit.length; i++) {
+            bestProfit[difficulty[i]] = Math.max(bestProfit[difficulty[i]], profit[i]);
+        }
+
+        // find max profit for range [minDifficulty, difficulty[i]]
+        int maxProfit = 0;
+        for (int i = 0; i < bestProfit.length; i++) {
+            if (bestProfit[i] > maxProfit) {
+                maxProfit = bestProfit[i];
+            }
+            bestProfit[i] = maxProfit;
+        }
+
+        // calculate max profit for all worker
+        int result = 0;
+        for (int w: worker) {
+            if (w > maxDifficulty) {
+                result += bestProfit[maxDifficulty];
+            } else {
+                result += bestProfit[w];
+            }
+        }
+        return result;
+    }
+}
