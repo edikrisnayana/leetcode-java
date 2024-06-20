2957894646e4db9a76fc1d6e1418ccebddec73d5
Krisna <ejikrisnayana@gmail.com>
2024-06-20 08:40:16 +0700

    1482. Minimum Number of Days to Make m Bouquets

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/src/Solution.java
@@ -0,0 +1,46 @@
+public class Solution {
+    private int[] arr;
+    private int m, k;
+
+    public int minDays(int[] bloomDay, int m, int k) {
+        int len = bloomDay.length;
+        if ((long) m * k > len) {
+            return -1;
+        }
+
+        int max = 0;
+        for (int day : bloomDay) {
+            if (max < day) {
+                max = day;
+            }
+        }
+
+        int left = 1;
+        int right = max;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (!count(bloomDay, mid, m, k)) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
+    private boolean count(int[] bloomDay, int val, int m, int k) {
+        int flower = 0;
+        int bouq = 0;
+        for (int day : bloomDay) {
+            if (day > val) {
+                flower = 0;
+            } else if (++flower >= k) {
+                bouq++;
+                flower = 0;
+            }
+        }
+        return bouq >= m;
+
+    }
+}
