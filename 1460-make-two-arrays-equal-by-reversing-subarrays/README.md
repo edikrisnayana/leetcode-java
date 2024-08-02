37dc266c262f62817b236bca60925fe89f624c50
Krisna Windows <ejikrisnayana@gmail.com>
2024-08-03 01:26:15 +0700

    1460. Make Two Arrays Equal by Reversing Subarrays

---
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/README.md
@@ -0,0 +1,3 @@
+## 1460. Make Two Arrays Equal by Reversing Subarrays
+
+Question link: https://leetcode.com/problems/make-two-arrays-equal-by-reversing-subarrays/description/
---
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/src/Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        int[] map = new int[1001];
+        for (int num : target) {
+            map[num]++;
+        }
+
+        for (int num : arr) {
+            map[num]--;
+            if (map[num] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
