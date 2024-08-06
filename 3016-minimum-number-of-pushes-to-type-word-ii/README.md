698349eadd47c4185f503c953375fa687f2cce2e
Krisna <ejikrisnayana@gmail.com>
2024-08-06 07:35:32 +0700

    3016. Minimum Number of Pushes to Type Word II

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/README.md
@@ -0,0 +1,3 @@
+## 3016. Minimum Number of Pushes to Type Word II
+
+Question link: https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-ii/description/
---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        Object[][] arguments = new Object[3][2];
+        arguments[0][0] = "abcde";
+        arguments[0][1] = 5;
+
+        arguments[1][0] = "xyzxyzxyzxyz";
+        arguments[1][1] = 12;
+
+        arguments[2][0] = "aabbccddeeffgghhiiiiii";
+        arguments[2][1] = 24;
+
+        for (int i = 0; i < arguments.length; i++) {
+            System.out.printf("Test Case %s:\n", i + 1);
+            Solution solution = new Solution();
+            String word = (String) arguments[i][0];
+            int expected = (int) arguments[i][1];
+            System.out.print("input: ");
+            System.out.println(word);
+            System.out.print("output: ");
+            int output = solution.minimumPushes(word);
+            System.out.println(output);
+            System.out.print("expected: ");
+            System.out.println(expected);
+            System.out.println("match: " + (output == expected));
+            System.out.println();
+        }
+    }
+}
