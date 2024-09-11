b277ae3996b7af16fed2b0c78d70f00aebb2c113
Krisna Windows <ejikrisnayana@gmail.com>
2024-09-11 07:17:26 +0700

    2220. Minimum Bit Flips to Convert Number

---
+++ 2220-minimum-bit-flips-to-convert-number/README.md
@@ -0,0 +1,3 @@
+## 2220. Minimum Bit Flips to Convert Number
+
+Question link: https://leetcode.com/problems/minimum-bit-flips-to-convert-number/description/
---
+++ 2220-minimum-bit-flips-to-convert-number/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+  public static void main(String[] args) {
+    Object[][] arguments = new Object[2][3];
+    arguments[0][0] = 10;
+    arguments[0][1] = 7;
+    arguments[0][2] = 3;
+    
+    arguments[1][0] = 3;
+    arguments[1][1] = 4;
+    arguments[1][2] = 3;
+    
+    for (int i = 0; i < arguments.length; i++) {
+      System.out.printf("Test Case %s:\n", i + 1);
+      Solution solution = new Solution();
+      int start = (int) arguments[i][0];
+      int goal = (int) arguments[i][1];
+      int expected = (int) arguments[i][2];
+      System.out.println("input: ");
+      System.out.println(start);
+      System.out.println(goal);
+      System.out.print("output: ");
+      int output = solution.minBitFlips(start, goal);
+      System.out.println(output);
+      System.out.print("expected: ");
+      System.out.println(expected);
+      System.out.println("match: " + (output == expected));
+      System.out.println();
+    }
+  }
+}
