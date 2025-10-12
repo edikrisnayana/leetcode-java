becaecf357e3e610be05125832447199e8011ae0
Krisna <ejikrisnayana@gmail.com>
2025-10-12 19:03:02 +0700

    303. Range Sum Query - Immutable

---
+++ 303-range-sum-query-immutable/README.md
@@ -0,0 +1,3 @@
+## 303. Range Sum Query - Immutable
+
+Question link: https://leetcode.com/problems/range-sum-query-immutable/description/
---
+++ 303-range-sum-query-immutable/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+  public static void main(String[] args) {
+    NumArray numArray = new NumArray(new int[]{-2, 0, 3, -5, 2, -1});
+    System.out.println(numArray.sumRange(0, 2));
+    System.out.println(numArray.sumRange(2, 5));
+    System.out.println(numArray.sumRange(0, 5));
+  }
+}
